181de8b4fc2e9ec94e5f384d9338439faed27534
dldhk97 <dldhk97@gmail.com>
2022-02-14 17:27:32 +0900

    fix Headless option

--- autochecker/driver/loader.py
+++ autochecker/driver/loader.py
@@ -27,8 +27,8 @@ def _get_options():
     options = webdriver.ChromeOptions()
     
     # Enable headless mode
-    # options.add_argument('headless')
-    # options.add_argument("no-sandbox")
+    options.add_argument('headless')
+    options.add_argument("no-sandbox")
 
     options.add_argument("disable-gpu")
     options.add_argument("lang=ko_KR")
