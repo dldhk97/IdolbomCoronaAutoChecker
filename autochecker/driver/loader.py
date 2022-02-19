87652bc5948db01af94b6469c867a8fe7ca28fe4
dldhk97 <dldhk97@gmail.com>
2022-02-20 00:18:24 +0900

    .env에 HEADLESS_MODE 추가

--- autochecker/driver/loader.py
+++ autochecker/driver/loader.py
@@ -27,8 +27,9 @@ def _get_options():
     options = webdriver.ChromeOptions()
     
     # Enable headless mode
-    options.add_argument('headless')
-    options.add_argument("no-sandbox")
+    if os.environ.get('HEADLESS_MODE') == 'True':
+        options.add_argument('headless')
+        options.add_argument("no-sandbox")
 
     options.add_argument("disable-gpu")
     options.add_argument("lang=ko_KR")
